72529880484869471337461e5106621d1787684a
Suvra Deb Paul | Web Developer <suvra.deb.paul@gmail.com>
2026-07-07 01:41:17 +0600

    Added Category,service,booking,review Schema

---
+++ prisma/migrations/20260706193941_new_schema/migration.sql
@@ -0,0 +1,116 @@
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('REQUESTED', 'ACCEPTED', 'DECLINED', 'PAID', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED');
+
+-- CreateTable
+CREATE TABLE "bookings" (
+    "id" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "technicianId" TEXT NOT NULL,
+    "serviceId" TEXT NOT NULL,
+    "scheduleTime" TIMESTAMP(3) NOT NULL,
+    "address" TEXT NOT NULL,
+    "status" "BookingStatus" NOT NULL DEFAULT 'REQUESTED',
+    "totalAmount" DECIMAL(65,30) NOT NULL,
+    "cancelReason" TEXT,
+    "completedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "bookings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" TEXT NOT NULL,
+    "bookingId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "technicianId" TEXT NOT NULL,
+    "rating" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "comment" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "services" (
+    "id" TEXT NOT NULL,
+    "technicanId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "tittle" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "services_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bookings_customerId_key" ON "bookings"("customerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bookings_technicianId_key" ON "bookings"("technicianId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bookings_serviceId_key" ON "bookings"("serviceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_slug_key" ON "categories"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reviews_bookingId_key" ON "reviews"("bookingId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reviews_customerId_key" ON "reviews"("customerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reviews_technicianId_key" ON "reviews"("technicianId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "services_technicanId_key" ON "services"("technicanId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "services_categoryId_key" ON "services"("categoryId");
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customer_profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_technicianId_fkey" FOREIGN KEY ("technicianId") REFERENCES "technician_profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "services"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "bookings"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customer_profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_technicianId_fkey" FOREIGN KEY ("technicianId") REFERENCES "technician_profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "services" ADD CONSTRAINT "services_technicanId_fkey" FOREIGN KEY ("technicanId") REFERENCES "technician_profiles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "services" ADD CONSTRAINT "services_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
