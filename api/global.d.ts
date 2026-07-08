329a064162b2f61b71d82d7f69825d43cde74c99
Suvra Deb Paul | Web Developer <suvra.deb.paul@gmail.com>
2026-07-08 20:06:33 +0600

    Vercel Setting Change

--- api/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "../dist-vercel/app.js" {
-  import { Application } from "express";
-  const app: Application;
-  export default app;
-}
--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import app from "../dist-vercel/app.js";
 
 export default app;
